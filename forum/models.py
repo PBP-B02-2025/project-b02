2c8d0f0b20ccdd21de5cb2cf6849c27ef322dea1
JaaTzy14 <mirzaradithya1410@gmail.com>
2025-10-21 21:49:46 +0700

    Membuat model forum

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
+import uuid
 # Create your models here.
+
+class Forum(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    forum_views = models.IntegerField(default=0)
+    content = models.TextField()
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    
+    def increment_views(self):
+        self.forum_views += 1
+        self.save()
+    
+    def updateForum(self):
+        self.updated_at = timezone.now()
+        self.save()
