13831d3ef85d681541c7c69936ed2464ab9e9e39
JaaTzy14 <mirzaradithya1410@gmail.com>
2025-10-22 10:52:08 +0700

    Menambahkan fungsi JSON

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from forum.views import show_forum
+from forum.views import show_forum, show_json_forum, show_json_comment
 
 app_name = 'forum'
 
 urlpatterns = [
     path('', show_forum, name='show_forum'),
+    path('json-forum/', show_json_forum, name='show_json'),
+    path('json-comment/<uuid:id>', show_json_comment, name='show_json_comment'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-
+from forum.models import Forum, Comment
+from django.http import JsonResponse
 # Create your views here.
 
 def show_forum(request):
     return render(request, "forum.html", {})
+
+def show_json_forum(request):
+    forum_list = Forum.objects.all()
+    forum_data = [
+        {
+            'id': str(forum.id),
+            'title': forum.title,
+            'author': forum.author.username,
+            'content': forum.content,
+            'created_at': forum.created_at.isoformat(),
+            'updated_at': forum.updated_at.isoformat(),
+            'views': str(forum.forum_views),
+        } for forum in forum_list
+    ]
+    return JsonResponse(forum_data, safe=False)
+
+def show_json_comment(request, id):
+    comment_list = Comment.objects.filter(forum_id=id)
+    comment_data = [
+        {
+            'id': str(comment.id),
+            'author': comment.author.username,
+            'content': comment.content,
+            'created_at': comment.created_at.isoformat(),
+            'updated_at': comment.updated_at.isoformat(),
+        } for comment in comment_list
+    ]
+    return JsonResponse(comment_data, safe=False)
