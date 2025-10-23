42e688f11506ffd51d3f37d238a711524ecb8c4c
AufaSatriatama <aufa4satriatama@gmail.com>
2025-10-23 21:01:39 +0700

    update urls and add .html files

--- review/urls.py
+++ review/urls.py
@@ -4,7 +4,7 @@ app_name = 'review'
 
 urlpatterns = [
     path('review/add-review', add_review, name='add_review'),
-    path('review/edit-review', edit_review, name='edit_review'),
-    path('review/delete-review', delete_review, name='delete_review'),
-    path('review/read-review', read_review_by_json, name='read_review_by_json'),
+    path('review/<uuid:id>/edit', edit_review, name='edit_review'),
+    path('review/<uuid:id>/delete', delete_review, name='delete_review'),
+    path('review/<uuid"id>/read', read_review_by_json, name='read_review_by_json'),
 ]
