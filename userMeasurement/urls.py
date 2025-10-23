b6ed016355263e69059ecf6ee47648225481b1cb
Jovian12 <infernum212@gmail.com>
2025-10-23 22:43:00 +0700

    Ubah tampilan

--- userMeasurement/urls.py
+++ userMeasurement/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from userMeasurement.views import show_measurement, update_measurement
+from userMeasurement.views import show_measurement, update_measurement, delete_measurement
 
 app_name = 'userMeasurement'
 
 urlpatterns = [
     path('', show_measurement, name='show_measurement'),
     path('update/', update_measurement, name='update_measurement'),
+    path('delete/', delete_measurement, name='delete_measurement'),
 ]
--- userMeasurement/views.py
+++ userMeasurement/views.py
@@ -3,16 +3,21 @@ from userMeasurement.forms import UserMeasurementForm
 from userMeasurement.models import userMeasurement
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from shop.models import Product
+from django.contrib import messages
 # Create your views here.
 @login_required(login_url='/login')
 def show_measurement(request):
     user = request.user
     data = userMeasurement.objects.filter(user=user).first()
+    product = []
 
-    if data is None:
+    if data is None or data.recommended_size is None:
         return render(request, 'recommended_size.html', {'data': None})
 
-    return render(request, 'recommended_size.html', {'data': data})
+    else :
+        product = Product.objects.filter(size = data.recommended_size)
+        return render(request, 'recommended_size.html', {'data': data, 'product' : product})
 
 @login_required(login_url='/login')
 def update_measurement(request):
@@ -31,6 +36,20 @@ def update_measurement(request):
         form = UserMeasurementForm(instance=data)
 
     return render(request, 'form.html', {'form': form})
+
+@login_required(login_url='/login')
+def delete_measurement(request):
+    user = request.user
+    data = userMeasurement.objects.filter(user=user).first()
+
+    if data:
+        data.delete()
+        messages.success(request, "Data ukuran badan berhasil dihapus.")
+    else:
+        messages.warning(request, "Tidak ada data untuk dihapus.")
+
+    return redirect('userMeasurement:show_measurement')
+
 
 
 
