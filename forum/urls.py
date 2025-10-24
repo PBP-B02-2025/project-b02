ce5680f4fad494c6f8dc4da09323fa85a3521ba7
JaaTzy14 <mirzaradithya1410@gmail.com>
2025-10-24 18:11:53 +0700

    memperbaiki redundant

--- forum/urls.py
+++ forum/urls.py
@@ -6,7 +6,6 @@ app_name = 'forum'
 urlpatterns = [
     path('', show_forum_list, name='show_forum_list'),
     path('json-forum/', show_json_forum, name='show_json'),
-    path('create-ajax/', create_forum_ajax, name='create_forum_ajax'),
     path('<uuid:id>', show_forum, name='show_forum'),
     path('json-forum/<uuid:id>', show_json_forum_by_id, name='show_json_forum_by_id'),
     path('json-comment/<uuid:id>', show_json_comment, name='show_json_comment'),
